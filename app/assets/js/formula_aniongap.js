486c59899fae23b47d7c78c784479fc2acad796f
Mohammad Fidaali <mohammad.fidaali@gmail.com>
2017-05-02 01:09:01 -0700

    Updated color of error'd inputs and minor other changes

--- app/assets/js/formula_aniongap.js
+++ app/assets/js/formula_aniongap.js
@@ -1,6 +1,6 @@
 var normbereichHash = {
-    "with_potassium"     : "11 \xB1 4 mmol/L",
-    "without_potassium"  : "7 \xB1 4 mmol/L"
+    "with_potassium"     : "7 \u2014 15 mmol/L",
+    "without_potassium"  : "3 \u2014 11 mmol/L"
 };
 
 document.addEventListener('DOMContentLoaded', function() {
